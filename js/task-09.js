1b5cc40c11e0c86db1ad7259500f21e6e32acacf
wellkornilov <kornilovaleriy@gmail.com>
2022-11-14 18:24:34 +0200

    555

--- js/task-09.js
+++ js/task-09.js
@@ -1,39 +1,29 @@
 // отримуємо доступ до елементів HTML
-const changeColorButton = document.querySelector('.change-color');
-const bodyEl = document.querySelector('body');
-const textEl = document.querySelector('.color');
-
+const changeColorButton = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+const textEl = document.querySelector(".color");
 
 // додаємо івент-лісенер
-changeColorButton.addEventListener('click', onButtonClick);
+changeColorButton.addEventListener("click", onButtonClick);
 
 // function onButtonClick() {
 //   function getRandomHexColor() {
 //     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 // };
-//   // додаємо згенероавний рандомний колір в інлайн-стиль елемента body 
+//   // додаємо згенероавний рандомний колір в інлайн-стиль елемента body
 //   bodyEl.style.backgroundColor = getRandomHexColor();
 //   // додаємо значення кольору в span
 //   textEl.textContent = bodyEl.style.backgroundColor;
 // };
 
-
-
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 function onButtonClick() {
-  const color = getRandomHexColor()
-  // додаємо згенероавний рандомний колір в інлайн-стиль елемента body 
+  const color = getRandomHexColor();
+  // додаємо згенероавний рандомний колір в інлайн-стиль елемента body
   bodyEl.style.backgroundColor = color;
   // додаємо значення кольору в span
   textEl.textContent = color;
-};
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-St
+}
--- js/task-10.js
+++ js/task-10.js
@@ -1,8 +1,36 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-const controlEl = document.querySelector("#controls");
+const btnCreateEl = document.querySelector("#controls button[data-create");
+const btnDestroyEl = document.querySelector("#controls button[data-destroy");
+const inputEl = document.querySelector("#controls input");
 const boxesEl = document.querySelector("#boxes");
+const controlsEl = document.querySelector("#controls");
 
-controlEl.addEventListener("input");
+btnCreateEl.addEventListener("click", onBtnCreateEl);
+btnDestroyEl.addEventListener("click", onBtnDestroyEl);
+
+function onBtnDestroyEl() {
+  boxesEl.innerHTML = "";
+  inputEl.value = "";
+}
+
+function onBtnCreateEl() {
+  createBoxes(Number(inputEl.value));
+}
+
+function createBoxes(amount) {
+  let markup = ``;
+
+  for (let i = 0; i < amount; i += 1) {
+    markup += `<div style="background-color: ${getRandomHexColor()}"></div>`;
+  }
+
+  boxesEl.insertAdjacentHTML("afterbegin", markup);
+
+  const arr = [...boxesEl.children];
+  arr.forEach((div, index) => {
+    div.style.width = `${30 + 10 * (index + 1)}px`;
+    div.style.height = `${30 + 10 * (index + 1)}px`;
+  });
+}
